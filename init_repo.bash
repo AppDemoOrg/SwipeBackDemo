fbc9958605b4b2716cf89a645849cad009e5c1e0
hwq <weiqi.huang@pisofttech.com>
2018-04-23 15:56:39 +0800

    init repo

---
+++ init_repo.bash
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+# git status
+echo 'git status'
+git status
+
+
+# git add
+echo 'git add'
+git add .
+
+
+# git commit
+echo 'git commit -m "init repo"'
+git commit -m "init repo"
+
+
+# git push 
+echo 'git push'
+git push
+
+# end
+echo 'bash end ...'
+
